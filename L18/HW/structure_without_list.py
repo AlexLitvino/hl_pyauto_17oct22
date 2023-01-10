b53e800ca918a9b92a6c5649971606102dedf524
AlexLitvino <litvinov.aleks@gmail.com>
2023-01-10 19:35:30 +0200

    made class fields protected

--- L18/HW/structure_without_list.py
+++ L18/HW/structure_without_list.py
@@ -10,32 +10,32 @@ class StructureWithoutList:
     def add(self, value):
         """ Adding new element """
         if self.length == 0:
-            self.current_item = Item(value, prev_item=None, next_item=None)
+            self._current_item = Item(value, prev_item=None, next_item=None)
         else:
-            new_item = Item(value, prev_item=self.current_item, next_item=None)
-            self.current_item.next_item = new_item
-            self.current_item = new_item
+            new_item = Item(value, prev_item=self._current_item, next_item=None)
+            self._current_item.next_item = new_item
+            self._current_item = new_item
         self.length += 1
-        self.current_index += 1
+        self._current_index += 1
 
     def get(self, index):
         """ Getting value by index """
         if 1 <= index <= self.length:
-            delta = self.current_index - index
+            delta = self._current_index - index
             if delta >= 0:  # current_index > index, we ahead of destination
                 for i in range(delta):
                     self._go_previous()
             else:
                 for i in range(-delta):
                     self._go_next()
-            return self.current_item.value
+            return self._current_item.value
         else:
             raise IndexError
 
     def delete(self, index):
         """ Deleting element by index """
         if 1 <= index <= self.length:
-            delta = self.current_index - index
+            delta = self._current_index - index
             if delta >= 0:   # current_index > index, we ahead of destination
                 for i in range(delta):
                     self._go_previous()
@@ -46,26 +46,26 @@ class StructureWithoutList:
             if self.length == 1:
                 self._reset()
             else:
-                if self.current_index == 1:
-                    next_item = self.current_item.next_item
-                    del self.current_item
-                    self.current_item = next_item
-                    self.current_item.prev_item = None
-                    self.current_index = 1
-                elif self.current_index == self.length:
-                    prev_item = self.current_item.prev_item
-                    del self.current_item
-                    self.current_item = prev_item
-                    self.current_item.next_item = None
-                    self.current_index = self.length - 1
+                if self._current_index == 1:
+                    next_item = self._current_item.next_item
+                    del self._current_item
+                    self._current_item = next_item
+                    self._current_item.prev_item = None
+                    self._current_index = 1
+                elif self._current_index == self.length:
+                    prev_item = self._current_item.prev_item
+                    del self._current_item
+                    self._current_item = prev_item
+                    self._current_item.next_item = None
+                    self._current_index = self.length - 1
                 else:
-                    prev_item = self.current_item.prev_item
-                    next_item = self.current_item.next_item
+                    prev_item = self._current_item.prev_item
+                    next_item = self._current_item.next_item
                     prev_item.next_item = next_item
                     next_item.prev_item = prev_item
-                    del self.current_item
-                    self.current_item = prev_item
-                    self.current_index -= 1
+                    del self._current_item
+                    self._current_item = prev_item
+                    self._current_index -= 1
                 self.length -= 1
         else:
             raise IndexError
@@ -73,20 +73,20 @@ class StructureWithoutList:
     def _reset(self):
         """ Sets collection initial values at the initialization and when removing the only element """
         self.length = 0
-        self.current_item = None
-        self.current_index = 0
+        self._current_item = None
+        self._current_index = 0
 
     def _go_previous(self):
         """ Go to previous item in collection """
-        prev_item = self.current_item.prev_item
-        self.current_item = prev_item
-        self.current_index -= 1
+        prev_item = self._current_item.prev_item
+        self._current_item = prev_item
+        self._current_index -= 1
 
     def _go_next(self):
         """ Go to next item in collection """
-        next_item = self.current_item.next_item
-        self.current_item = next_item
-        self.current_index += 1
+        next_item = self._current_item.next_item
+        self._current_item = next_item
+        self._current_index += 1
 
 
 class Item:
